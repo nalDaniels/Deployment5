1a08fe5d9316a24dd2e14123db6335c6002a597e
Nalani Daniels <nalanidaniels23@gmail.com>
2023-10-14 05:45:03 +0000

    Updated setup2.sh and added terraform files

--- setup2.sh
+++ setup2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm -rf Deployment5
 source test/bin/activate
-git clone https://github.com/kura-labs-org/c4_deployment-5.git
-cd c4_deployment-5
+git clone https://github.com/nalDaniels/Deployment5.git
+cd Deployment5
 pip install -r requirements.txt
 pip install gunicorn
 python database.py
---
+++ variables.tf
@@ -0,0 +1,35 @@
+##### VARIABLES FOR PROVIDER BLOCK #####
+variable "access_key" {} 
+variable "secret_key" {} 
+
+##### VARIABLES FOR VPC BLOCK #####
+variable "vpcname" {} 
+
+##### VARIABLES FOR SUBNET BLOCK #####
+variable "subnet1name" {} 
+variable "subnet1AZ" {} 
+variable "subnet2name" {}
+variable "subnet2AZ" {} 
+
+
+
+##### VARIABLES FOR SECURITY GROUP BLOCK #####
+variable "SGName1" {} 
+variable "SGName2" {} 
+
+
+##### VARIABLES FOR INTERNET GATEWAY BLOCK #####
+variable "IGName" {} 
+
+##### VARIABLES FOR ROUTE TABLE BLOCK #####
+variable "RTname" {} 
+
+
+
+##### VARIABLES FOR INSTANCE BLOCK #####
+variable "ami" {} 
+variable "instance_type" {} 
+variable "key_name" {} 
+variable "InstanceName1" {} 
+variable "InstanceName2" {} 
+
